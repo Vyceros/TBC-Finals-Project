9b718916c91eb4f2b8f00de0e252173f377f9cd2
RatiMghvdeladze <mghvdeladze.rati@gmail.com>
2025-04-20 20:39:29 +0400

    added splash screen momentum

--- app/src/main/java/ge/fitness/momentum/MainActivity.kt
+++ app/src/main/java/ge/fitness/momentum/MainActivity.kt
@@ -5,7 +5,6 @@ import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
-import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
