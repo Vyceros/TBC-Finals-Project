abb1e2eda136d6db35d840abd3f09852c8a68087
Vyceros <luka.kurashvili.1@btu.edu.ge>
2025-04-22 20:56:44 +0400

    Database module

---
+++ core/database/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.momentum.application.library)
+    alias(libs.plugins.momentum.room)
+    alias(libs.plugins.momentum.hilt)
+}
+
+android {
+    namespace = "ge.fitness.core.database"
+}
+
+dependencies {
+}
