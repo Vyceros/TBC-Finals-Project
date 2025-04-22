aa50846e53ef108971f33cf9098d3d73bf5dc856
Vyceros <luka.kurashvili.1@btu.edu.ge>
2025-04-22 21:02:24 +0400

    redundancy removed -> very MINOR change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.core.ktx)
